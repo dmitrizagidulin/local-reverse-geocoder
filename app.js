113681a893c6df4d0f87e0df2f862be27af3832e
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-02-23 18:55:03 -0500

    Implement simple shortname() function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var geocoder = require('./geocoder.js');
 
-app.get(/geocode/, function(req, res) {
+function getParams (req, res) {
   var lat = req.query.latitude || false;
   var lon = req.query.longitude || false;
   var maxResults = req.query.maxResults || 1;
@@ -22,15 +22,50 @@ app.get(/geocode/, function(req, res) {
   } else {
     points[0] =  {latitude: lat, longitude: lon};
   }
-  geocoder.lookUp(points, maxResults, function(err, addresses) {
+  return { points, maxResults };
+}
+
+/**
+ *
+ * @param addresses
+ */
+function getShortname (addresses) {
+  const { asciiName, countryCode, admin1Code } = addresses.flat()[0];
+  const shortname = `${asciiName}, ${admin1Code.asciiName}, ${countryCode}`;
+  return shortname;
+}
+
+app.get(/shortname/, (req, res) => {
+  const { points } = getParams(req, res);
+  const maxResults = 1;
+
+  geocoder.lookUp(points, maxResults, (err, addresses) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(400).send(err);
+    }
+    let locationName = {};
+    try {
+      locationName = getShortname(addresses);
+    } catch (err) {
+      console.log(err);
+    }
+
+    return res.send(locationName);
+  });
+});
+
+app.get(/geocode/, (req, res) => {
+  const { points, maxResults } = getParams(req, res);
+
+  geocoder.lookUp(points, maxResults, (err, addresses) => {
+    if (err) {
+      return res.status(400).send(err);
     }
     return res.send(addresses);
   });
 });
 
-geocoder.init({}, function() {
+geocoder.init({}, () => {
   var port = Number(process.env.PORT || 3000);
   app.listen(port, function() {
     console.log('Local reverse geocoder listening on port ' + port);
